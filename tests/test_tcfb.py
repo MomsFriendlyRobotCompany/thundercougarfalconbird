15f1a797603d7f2ab66ffa21c38106ade7ed49d7
walchko <walchko@users.noreply.github.com>
2022-08-16 14:47:13 -0600

    testing

--- tests/test_tcfb.py
+++ tests/test_tcfb.py
@@ -1,7 +1,19 @@
 import pytest
 from thundercougarfalconbird import *
 
-def test_default():
+
+def test_wheeled():
     r = DifferentialDriveKinematics(1,2)
+
+    with pytest.raises(TypeError):
+        r = DifferentialDriveKinematics()
+
     r = RoombaCreate()
     assert True
+
+
+def test_uav():
+    assert ParrotDrone()
+
+    with pytest.raises(Exception):
+        r = Drone()
