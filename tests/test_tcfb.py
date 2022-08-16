97cb124b158b922e969e5db68558e48e2cc0c6a0
walchko <walchko@users.noreply.github.com>
2022-08-16 08:49:23 -0600

    init

---
+++ tests/test_tcfb.py
@@ -0,0 +1,4 @@
+import pytest
+
+def test_default():
+    assert True
---
+++ thundercougarfalconbird/__init__.py
@@ -0,0 +1,5 @@
+from importlib.metadata import version
+
+__license__ = 'MIT'
+__author__ = 'Kevin Walchko'
+__version__ = version("thundercougarfalconbird")
